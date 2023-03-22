323527aceac540d9a3c39bfd183ca8f74db70a6c
Adithya K Shetty <adithyakshetty01@gmail.com>
2023-03-22 17:09:31 +0530

    Implemented Splash Screen

--- app/src/main/java/com/example/signify/MainActivity.java
+++ app/src/main/java/com/example/signify/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.signify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.example.signify.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    Animation topAni,bottomAni;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        topAni = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAni = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        binding.appLogo.setAnimation(topAni);
+        binding.appName.setAnimation(bottomAni);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,Welcome.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
     }
 }
