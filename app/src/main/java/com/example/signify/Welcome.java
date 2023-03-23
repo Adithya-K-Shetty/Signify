4357c197e2625add9355244296fcb755bb55f3e7
Adithya K Shetty <adithyakshetty01@gmail.com>
2023-03-23 16:31:33 +0530

    Implemented Welcome Board Page

--- app/src/main/java/com/example/signify/Welcome.java
+++ app/src/main/java/com/example/signify/Welcome.java
@@ -3,12 +3,21 @@ package com.example.signify;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Welcome extends AppCompatActivity {
-
+    Animation car_animation;
+    ImageView car_image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        car_animation = AnimationUtils.loadAnimation(this,R.anim.car_bottom_to_top);
+        car_image =(ImageView) findViewById(R.id.carImage);
+        car_image.setAnimation(car_animation);
+
     }
 }
