2ff985927dce9292ada4b13f9ca548b12ae617ed
avi7501 <avinashmuralidharan2@gmail.com>
2023-04-22 17:26:26 +0530

    fixed bugs and added custom car map marker

--- app/src/main/java/com/example/signify/MapsFragment.java
+++ app/src/main/java/com/example/signify/MapsFragment.java
@@ -2,21 +2,48 @@ package com.example.signify;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
 
 public class MapsFragment extends Fragment {
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
+    private PlacesClient placesClient;
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
@@ -31,9 +58,37 @@ public class MapsFragment extends Fragment {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//            LatLng sydney = new LatLng(-34, 151);
+//            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            getContext(), R.raw.style_json));
+            mMap = googleMap;
+            // Add your customizations to the map
+
+            // Get the current location
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            } else {
+                fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(getActivity(), location -> {
+                            if (location != null) {
+                                // Update the map with the current location
+                                LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                                mMap.addMarker(new MarkerOptions().icon(bitmapDescriptorFromVector(getContext(),R.drawable.carmarker)).position(currentLocation).title("your location"));
+                                CameraPosition cameraPosition = new CameraPosition.Builder()
+                                        .target(currentLocation )
+                                        .zoom(18)
+//                                        .bearing(90)
+//                                        .tilt(30)
+                                        .build();
+                                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+                            }
+                        });
+            }
         }
     };
 
@@ -50,8 +105,93 @@ public class MapsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+//        // Initialize Google Places API client
+//        PlacesClient placesClient = Places.createClient(getContext());
+//        if (!Places.isInitialized()) {
+//            Places.initialize(requireContext().getApplicationContext(), "AIzaSyCYO6R9qg5s6eUi0-a_m9851hiYX_UGsL0");
+//        }
+//// Initialize FusedLocationProviderClient
+//        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+//
+//// Get the current user's location
+//        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//            fusedLocationProviderClient.getLastLocation().addOnSuccessListener((location) -> {
+//                if (location != null) {
+//                    // Use the user's location to search for nearby mechanics
+//                    LatLng userLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+//                    LatLngBounds bounds = LatLngBounds.builder().include(userLatLng).build();
+//                    FindAutocompletePredictionsRequest request = FindAutocompletePredictionsRequest.builder()
+//                            .setTypeFilter(TypeFilter.ESTABLISHMENT)
+//                            .setLocationBias(RectangularBounds.newInstance(bounds))
+//                            .setQuery("mechanic")
+//                            .build();
+//                    placesClient.findAutocompletePredictions(request).addOnSuccessListener((response) -> {
+//                        if (response.getAutocompletePredictions().size() > 0) {
+//                            // Get the details of the closest mechanic
+//                            AutocompletePrediction prediction = response.getAutocompletePredictions().get(0);
+//                            String placeId = prediction.getPlaceId();
+//                            FetchPlaceRequest placeRequest = FetchPlaceRequest.builder(placeId, Arrays.asList(Place.Field.LAT_LNG)).build();
+//                            placesClient.fetchPlace(placeRequest).addOnSuccessListener((placeResponse) -> {
+//                                LatLng mechanicLatLng = placeResponse.getPlace().getLatLng();
+//                                // Use the mechanic's location to show a marker on the map
+//                                mapFragment.getMapAsync((googleMap) -> {
+//                                    googleMap.addMarker(new MarkerOptions().position(mechanicLatLng).title("Mechanic"));
+//                                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mechanicLatLng, 15));
+//                                });
+//                            });
+//                        } else {
+//                            Toast.makeText(getContext(), "No nearby mechanics found", Toast.LENGTH_SHORT).show();
+//                        }
+//                    });
+//                } else {
+//                    Toast.makeText(getContext(), "Location not found", Toast.LENGTH_SHORT).show();
+//                }
+//            });
+//        } else {
+//            // Request location permission if not granted
+//            ActivityCompat.requestPermissions(requireActivity(), new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, REQUEST_LOCATION_PERMISSION);
+//        }
+
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
         }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+        // Create the location request
+        LocationRequest locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(5000)
+                .setFastestInterval(1000);
+
+        // Create the location callback
+        locationCallback = new LocationCallback() {
+
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+                for (Location location : locationResult.getLocations()) {
+                    // Update the map with the new location
+                    LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+                }
+            }
+        };
+
+        // Request location updates
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        } else {
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+        }
+    }
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectoreResId){
+        Drawable vectorDrawable= ContextCompat.getDrawable(context,vectoreResId);
+        vectorDrawable.setBounds(0,0,vectorDrawable.getIntrinsicWidth(),vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),vectorDrawable.getIntrinsicHeight(),Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 }
