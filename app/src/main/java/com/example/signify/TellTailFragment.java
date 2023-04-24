9e964b1a4b7bbca0d68c37fefaf2846020f3eb2b
avi7501 <avinashmuralidharan2@gmail.com>
2023-04-24 21:48:51 +0530

    integrated camera

--- app/src/main/java/com/example/signify/TellTailFragment.java
+++ app/src/main/java/com/example/signify/TellTailFragment.java
@@ -1,12 +1,44 @@
 package com.example.signify;
 
-import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.Manifest;
+import android.content.ContextWrapper;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +56,15 @@ public class TellTailFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    Button pred_btn;
+
+    Bitmap img;
+    public String all_signs;
+
+    public Dictionary signs_dict = new Hashtable();
+    public Boolean received_image = false;
+
+//    private DatabaseReference myRef;
     public TellTailFragment() {
         // Required empty public constructor
     }
@@ -53,12 +94,155 @@ public class TellTailFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 3){
+            img  = (Bitmap) data.getExtras().get("data");
+            received_image = true;
+//            int dimension = Math.min(image.getWidth(), image.getHeight());
+//            image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
+
+            if(received_image){
+                new MyAsyncTask().execute();
+            }
+
+
+//            image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+//            classifyImage(image);
+        }
+    }
+    private class MyAsyncTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            String postUrl = "http://192.168.43.101:5000/inputImage";
+
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            img.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+
+            RequestBody postBodyImage = new MultipartBody.Builder()
+                    .setType(MultipartBody.FORM)
+                    .addFormDataPart("image", "androidFlask.jpg", RequestBody.create(MediaType.parse("image/*jpg"), byteArray))
+                    .build();
+
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(postUrl)
+                    .post(postBodyImage)
+                    .build();
+
+            /*********** TESTING NEW STUFF ***************/
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    call.cancel();
+                    Log.d("FAIL", e.getMessage());
+                    System.out.println("FAIL : - "+ e.getMessage());
+                    System.out.println("Failed to Connect to Server");
+//                    runOnUiThread(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            System.out.println("Failed to Connect to Server");
+//                        }
+//                    });
+                }
+
+                @Override
+                public void onResponse(Call call, final Response response) throws IOException {
+                    // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+                    try {
+
+                        all_signs = response.body().string();
+                        System.out.println(all_signs);
+                        String[] all_signs_arr = all_signs.split("@");
+                        ArrayList<String> all_signs_list = new ArrayList<String>(
+                                Arrays.asList(all_signs_arr));
+//                        get_data(all_signs_list);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+//                            runOnUiThread(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    try {
+//                                        all_signs = response.body().string();
+//                                        System.out.println(all_signs);
+//                                    } catch (IOException e) {
+//                                        e.printStackTrace();
+//                                    }
+//                                }
+//                            });
+                }
+            });
+            /********** END OF TESTING ******************/
+
+            System.out.println(all_signs);
+            return all_signs;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            System.out.println("Hello World");
+            System.out.println(result);
+            System.out.println(all_signs);
+//            if (response_success){
+//                String[] all_signs_arr = all_signs.split("@");
+//                ArrayList<String> all_signs_list = new ArrayList<String>(
+//                        Arrays.asList(all_signs_arr));
+//                get_data(all_signs_list);
+//            }
+        }
+    }
+//    private void get_data(ArrayList<String> all_signs_list) {
+//        Query query = myRef.child("AllSigns");
+//
+//        query.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                for(DataSnapshot snapshot1 : snapshot.getChildren()){
+//                    if(all_signs_list.contains(snapshot1.child("signName").getValue().toString())){
+//                        signs_dict.put(snapshot1.child("signImageUrl").getValue().toString(),snapshot1.child("signDescription").getValue().toString());
+//                    }
+//                }
+//                System.out.println(signs_dict);
+////                String url = "";
+////                for(Enumeration enm = signs_dict.keys(); enm.hasMoreElements();)
+////                {
+//////prints the keys
+////                    url =  enm.nextElement().toString();
+////                    System.out.println("Hello");
+////                }
+////                System.out.println(url);
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tell_tail, container, false);
-    }
+        View view =inflater.inflate(R.layout.fragment_tell_tail, container, false);
+        Button cam_btn = (Button) view.findViewById(R.id.cameraBtn);
+        cam_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ActivityCompat.checkSelfPermission(getContext(),Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 3);
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+                }
+            }
+        });
+        return view;
+     }
 }
